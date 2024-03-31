972fc1a13890752a7e3433879008261a4feb7568
Pihla Jarv <pihla.jarv@gmail.com>
2024-03-31 23:57:20 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Application that enables to make HTTP Get-request to find out the food delivery fee based on vehicle type and current weather in the given city. Cron expression can be configured at application.properties file.
+Application that enables to make HTTP Get request to find out the food delivery fee based on vehicle type and current weather in the given city. 
+
+Cron expression can be configured at application.properties file.
+
+Unfortunately I didn't have time to write enough tests.
--- src/test/java/com/example/deliveryfeecalculator/DeliveryFeeCalculatorApplicationTests.java
+++ src/test/java/com/example/deliveryfeecalculator/DeliveryFeeCalculatorApplicationTests.java
@@ -1,16 +1,44 @@
 package com.example.deliveryfeecalculator;
 
+import com.example.deliveryfeecalculator.repository.RegionalBaseFeeRepository;
+import com.example.deliveryfeecalculator.repository.WeatherObservationRepository;
 import com.example.deliveryfeecalculator.service.DeliveryFeeCalculatingService;
+import com.example.deliveryfeecalculator.service.WeatherDataImportingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DeliveryFeeCalculatorApplicationTests {
+	@Autowired
+	WeatherDataImportingService weatherDataImportingService;
 
+	@Autowired
+	WeatherObservationRepository weatherObservationRepository;
 
+	@Autowired
+	DeliveryFeeCalculatingService deliveryFeeCalculatingService;
+
+	@Autowired
+	RegionalBaseFeeRepository regionalBaseFeeRepository;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void correctNumberOfWeatherObservations() throws ParserConfigurationException, URISyntaxException, IOException, SAXException {
+		weatherDataImportingService.importWeatherData();
+		weatherDataImportingService.importWeatherData();
+		long numberOfObservations = weatherObservationRepository.count();
+		assertEquals(6L, numberOfObservations);
 	}
 
 
